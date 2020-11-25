ebbcc40dbf51fc7d74b01c9714a8b780dc9d3e01
Ahmedchrifnasser <ahmed.chrif.nasr@gmail.com>
2020-11-25 23:18:23 +0100

    first commit

--- rocket-ship/src/routes/LaunchPad/components/Rocket/components/RocketCore.js
+++ rocket-ship/src/routes/LaunchPad/components/Rocket/components/RocketCore.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/_rocket.scss';
 
-const SECONDS_TO_TAKEOFF = 5;
+const SECONDS_TO_TAKEOFF = 10;
 const MS_TO_TAKEOFF = SECONDS_TO_TAKEOFF * 1000;
 const FINAL_POSITION_BOTTOM_VAL = 'calc(400px)';
 
 function timeToPositionPercent(startTime) {
   const now = Date.now();
-  const timeDiff = now - startTime;
+  const timeDiff = 0 /*now - startTime*/;
 
   if (timeDiff >= MS_TO_TAKEOFF) { return FINAL_POSITION_BOTTOM_VAL; }
 
